545f0c385d086428ba5a43d6326a60d00a2ae656
naomisalomon50-sys <naomi.salomon50@gmail.com>
2025-12-03 16:57:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Add the import statements for functions from string_utils.py and equation_utils.py here
-import string_utils *
-import equation_utils *
+
+from string_utils import parse_chemical_reaction
+from string_utils import count_atoms_in_reaction
+from equation_utils import build_equations
+from equation_utils import my_solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
